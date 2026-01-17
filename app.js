1e8df827a97bf73524aa9565bd0a1d0b8f318a9d
Tristin-TriloomLabs <tristin.h@triloomlabs.cloud>
2026-01-17 18:15:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ====== CONFIG ======
-const FLOW_GET_URL = "https://9f48aec3eda0e4b1bfc9a32779c7c8.4a.environment.api.powerplatform.com/powerautomate/automations/direct/workflows/f6e6dde5110e40d4b269c2d4a954b82c/triggers/manual/paths/invoke/survey?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KmgLO3Qspxo-l45H8BnXe6mj-ags27IOYXhA41NQxcQ"; // your GET flow
+const FLOW_GET_URL = "https://9f48aec3eda0e4b1bfc9a32779c7c8.4a.environment.api.powerplatform.com/powerautomate/automations/direct/workflows/f6e6dde5110e40d4b269c2d4a954b82c/triggers/manual/paths/invoke/survey?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KmgLO3Qspxo-l45H8BnXe6mj-ags27IOYXhA41NQxcQ&surveyId=22375dd3-9af3-f011-8406-002248f9b6c4&token=8ad6f5bd-d2a1-4091-bb10-96e23c9cd899"; // your GET flow
 const FLOW_POST_URL = "https://9f48aec3eda0e4b1bfc9a32779c7c8.4a.environment.api.powerplatform.com/powerautomate/automations/direct/workflows/a128a9d378bb429bb93a422e2529f7e3/triggers/manual/paths/invoke/submit?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QRr_Swy0AV8z7exa2wSkB9zL7Mf8tIxWFbPeaNn_8Pg"; // we'll add in Flow B
 // Optional: pass a surveyId in the email link like: ?surveyId=abc123
 // ====== END CONFIG ======
